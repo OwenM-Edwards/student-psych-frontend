51c95849535a5e11109868e8a944212c07f431c5
Zibbly <owenhedwards@gmail.com>
2021-02-11 01:22:04 +0000

    Add register/signin error consts.

--- src/redux/actions/action-types.js
+++ src/redux/actions/action-types.js
@@ -2,3 +2,5 @@ export const GET_ENTRIES = "GET_ENTRIES";
 export const ADD_ENTRY = "ADD_ENTRY";
 export const SIGN_IN = "SIGN_IN";
 export const REGISTER_USER = "REGISTER_USER";
+export const SIGN_IN_ERROR = "SIGN_IN_ERROR";
+export const REGISTER_USER_ERROR = "REGISTER_USER_ERROR";
