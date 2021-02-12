326c80dbaa0449c3c901489edf97cf457d92d326
Zibbly <owenhedwards@gmail.com>
2021-02-12 23:54:38 +0000

    Update CalenderSquare.js

--- src/components/CalenderSquare.js
+++ src/components/CalenderSquare.js
@@ -5,50 +5,40 @@ import {EventTag} from './index';
 
 const Wrapper = styled.div`
    width:100%;
+
    height:100%;
+   max-height:100%;
    text-align:center;
    &:hover {
-      border: 1px solid #2b2b2b;
+      scale:0.99;
    }
 `
-const StyledDayHeader = styled.h1`
-   font-size:1rem;
-`
+
 
 const CalenderSquare = ({ 
       setModalToggle,
-      setModModalToggle,
+      setaddModalToggle,
       setModalInfo, 
       entries, 
       calSquareDay,
       selectedDate,
+      userinfo,
+      createToast,
    }) => {
    const [isFetching, setIsFetching] = useState(false);
+   const [tagInfo, setTagInfo] = useState(false);
    let dayHeader = "";
    let eventInfo = false;
-   let dayStrings = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
    let calenderSquareProps = {
       backgroundColor:'',
+      opacity:1,
    }
    // Checks if calenderSquare day is a weekend.
-   let weekendCheck = new Date();
-   weekendCheck.setDate(calSquareDay);
+   let weekendCheck = new Date(selectedDate.year, selectedDate.month - 1, calSquareDay);
+   let currentDate = new Date();
+   let eventTag = [];
 
-
-   // Gets current entries in state, finds ones that match current date of calender square.
-   const sortEntries = () => {
-      if(entries){
-         entries.forEach(entry => {
-            if(entry.day === calSquareDay){
-               eventInfo = entry;
-            }
-         })
-      }
-   }
-   sortEntries();
-   
-   const handleTagClick = (e) => {
-      e.stopPropagation();
+   const handleTagClick = (eventInfo) => {
       if(eventInfo){
          setModalInfo({
             eventInfo,
@@ -57,36 +47,51 @@ const CalenderSquare = ({
       else {
          console.log('test')
       }
-      setModModalToggle(false);
+      setaddModalToggle(false);
       setModalToggle(true);
 
    }
    const handleBoxClick = (e) => {
       e.stopPropagation();
-      setModalInfo({
-         eventInfo:{
-            day:calSquareDay,
-            month:selectedDate.month,
-            year:selectedDate.year,
-         }
-      })  
-      setModalToggle(false);
-      setModModalToggle(true);
+      if(userinfo.id){
+         setModalInfo({
+            eventInfo:{
+               day:calSquareDay,
+               month:selectedDate.month,
+               year:selectedDate.year,
+            }
+         })  
+         setModalToggle(false);
+         setaddModalToggle(true);
+      }
    }
-
-
-   // If one of the first 7 days, add day string header.
-   if(calSquareDay < 8){
-      dayHeader = dayStrings[calSquareDay];
+   
+   // Gets current entries in state, finds ones that match current date of calender square.
+   const sortEntries = () => {
+      let count = 1;
+      if(entries){
+         entries.forEach(entry => {
+            if(entry.day === calSquareDay){
+               eventTag.push(
+                  <EventTag key={count} handleTagClick={handleTagClick} eventInfo={entry}/>
+               )
+               count++;
+            }
+         })
+      }
    }
+   sortEntries();
+
    // If box = outside total days in month
    if( calSquareDay > selectedDate.totalDaysInMonth){
       calenderSquareProps.backgroundColor = 'violet';
+      calenderSquareProps.opacity = 0;
    }
    // If box = current day
-   else if(calSquareDay == selectedDate.day){
+   else if(currentDate.getMonth() + 1 === selectedDate.month && calSquareDay == selectedDate.day){
       calenderSquareProps.backgroundColor = 'pink';
    }
+   
    // If box = A weekend
    else if(weekendCheck.getDay() === 6 || weekendCheck.getDay() === 0 ){
       calenderSquareProps.backgroundColor = 'purple';
@@ -97,17 +102,12 @@ const CalenderSquare = ({
 
 
    return(
-      <Wrapper onClick={handleBoxClick} style={{backgroundColor: calenderSquareProps.backgroundColor}}>
-         <StyledDayHeader>
-            {dayHeader}
-         </StyledDayHeader>
+      <Wrapper onClick={handleBoxClick} style={{backgroundColor: calenderSquareProps.backgroundColor, opacity:calenderSquareProps.opacity}}>
 
          {calSquareDay}
 
-         {(eventInfo)
-            ? <EventTag handleTagClick={handleTagClick} eventInfo={eventInfo}/>
-            : <div></div>
-         }
+         {eventTag}
+  
       </Wrapper>
    )
 
@@ -117,5 +117,5 @@ const CalenderSquare = ({
 }
 
 
-const mapStateToProps = (state) => ({ selectedDate:state.selectedDate.selectedDate, entries:state.entries.entries });
+const mapStateToProps = (state) => ({ userinfo:state.authenticate, selectedDate:state.selectedDate.selectedDate, entries:state.entries.entries });
 export default connect(mapStateToProps)(CalenderSquare);
