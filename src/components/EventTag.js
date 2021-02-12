b1d340be34861e86046d37dcc4ebc65d600d2b07
Zibbly <owenhedwards@gmail.com>
2021-02-12 23:54:44 +0000

    Update EventTag.js

--- src/components/EventTag.js
+++ src/components/EventTag.js
@@ -3,16 +3,25 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
    width:100%;
-   height:20px;
+   max-width:100%;
+   height:auto;
    background-color:white;
    cursor: pointer;
    z-index:2;
    font-size:1rem;
+   margin-bottom:5px;
+   padding:2px;
+   overflow-x:hidden;
 `
 
+
 const EventTag = ({ handleTagClick, eventInfo }) => {
+   const handleClick = (e) => {
+      e.stopPropagation();
+      handleTagClick(eventInfo);
+   }
    return(
-      <Wrapper onClick={handleTagClick}>
+      <Wrapper onClick={handleClick}>
          {eventInfo.title}
       </Wrapper>
    )
