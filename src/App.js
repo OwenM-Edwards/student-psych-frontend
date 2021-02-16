45e8d2ee3871cb8102464c33e9abe7e20a789a6e
Zibbly <owenhedwards@gmail.com>
2021-02-16 02:04:10 +0000

    Move some calender refresh code to main app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { getInitialDate, getEntries } from './redux/actions/index';
 import {
   BrowserRouter as Router, 
-  Switch,
   Route,
   Redirect,
 } from "react-router-dom";
 import styled from "styled-components";
 import { Calender, SignIn, Register } from './pages/index';
-import { Header } from './components/index';
+import { Header,LoadingIcon } from './components/index';
 import { connect } from 'react-redux';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from 'react-toastify';
 
 const Wrapper = styled.div`
   padding:5px;
@@ -28,49 +30,62 @@ const Wrapper = styled.div`
   & .main {
     height:90%;
     width:100%;
-    /* padding-right:5%; */
   }
 `
 
 
-const App = ({user}) => {
-  const loggedIn = user.token;
-
-  const regTest = () => {
-    let testString = "something@gmail.ac.uk";
-    let slicedTestString = testString.split('@');
-    let emailEnd = slicedTestString[1].toLowerCase();
-    console.log(`Test ${emailEnd}`);
-    let re = /ac\.uk/;
-    let reb = /^nhs\.uk$/;
-    console.log(re.test(emailEnd));
-  }
-  regTest();
+const App = ({addEntryState, getInitialDate, getEntries, auth, entries, selectedDate}) => {
+  useEffect(() => {
+    async function start(){
+      if(!selectedDate){
+        const initialDate = await getInitialDate();
+      }
+      if(selectedDate){
+        getEntries(selectedDate.month, selectedDate.year);
+      }
+    }
+    start();
+  }, [selectedDate, addEntryState.isFetching,]);
 
 
   return (
     <Router>
       <Wrapper>
+        <ToastContainer/>
         <div className="header">
           <Header/>
         </div>
 
+        <Route path="/maintenance">
+          <div>main</div>
+          
+        </Route>
+
         <Route path="/signin">
-          {(user.id)
-          ? <Redirect to="/"/>
-          : <SignIn/>
+          {(auth.user.id)
+            ? <Redirect to="/calender"/>
+            : <SignIn/>
           }
         </Route>
 
         <Route path="/register">
-          {(user.id)
-          ? <Redirect to="/"/>
-          : <Register/>
+          {(auth.user.id)
+            ? <Redirect to="/calender"/>
+            : <Register/>
+          }
+        </Route>
+
+        <Route path="/calender">
+          {(entries.isFetching)
+            ? <LoadingIcon/>
+            : (entries.entries)
+              ? <div className="main"> <Calender/> </div> 
+              : <div>maintenance</div>
           }
         </Route>
 
         <Route path="/">
-          <div className="main"> <Calender/> </div>
+          <Redirect to="/calender"/>
         </Route>
 
       </Wrapper>
@@ -79,5 +94,5 @@ const App = ({user}) => {
 }
 
 
-const mapStateToProps = (state) => ({ user:state.authenticate });
-export default connect(mapStateToProps)(App);
+const mapStateToProps = (state) => ({ addEntryState:state.addEntry, selectedDate:state.selectedDate.selectedDate, entries:state.entries, auth:state.authenticate });
+export default connect(mapStateToProps, {getInitialDate, getEntries})(App);
