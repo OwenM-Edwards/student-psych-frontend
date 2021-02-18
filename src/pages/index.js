60304f8b2e9fa8fef1f9bfa40bef2840d52b09f0
Zibbly <owenhedwards@gmail.com>
2021-02-18 13:44:09 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Calender from './Calender';
 import SignIn from './SignIn';
 import Register from './Register';
 import Verify from './Verify';
+import Search from './Search';
 
 export {
    Calender,
    SignIn,
    Register,
    Verify,
+   Search,
 }
