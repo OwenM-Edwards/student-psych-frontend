f304e0c3d3d430a504bec8addaff04db2dafd9a4
Zibbly <owenhedwards@gmail.com>
2021-02-21 15:46:24 +0000

    Adjust event tag color and position.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,18 +6,20 @@ import { connect } from 'react-redux';
 const Wrapper = styled.div`
    width:20%;
    max-width: 200px;
+   min-width:200px;
    height:100%;
    z-index:2;
    background: ${({ theme }) => theme.backgroundContrast};
    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
    text-align:center;
-   padding-top:50px;
+   padding-top:8%;
 `
 const EventsContainer = styled.div`
    width:100%;
-   height:50%;
+   min-height:30%;
    display:flex;
    flex-direction:column;
+   
    & .recentEventHeader{
       font-size:1.1rem;
       margin-bottom:15px;
