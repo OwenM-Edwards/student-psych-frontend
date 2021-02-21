ce66d3e300ac6b295db592f6f279028fc14c88b3
Zibbly <owenhedwards@gmail.com>
2021-02-21 19:33:27 +0000

    Update EventTag.js

--- src/components/EventTag.js
+++ src/components/EventTag.js
@@ -4,18 +4,20 @@ import styled from "styled-components";
 const Wrapper = styled.div`
    width:100%;
    max-height:46px;
-   cursor: pointer;
    z-index:2;
    font-size:1rem;
-   margin-bottom:5px;
-   padding:5px;
-   overflow:hidden;
-   word-wrap: break-word;
    color: ${({ theme }) => theme.contrastText};
    transition: all 0.2s ease-in-out;
-
+   & .tag {
+      padding:5px;
+      overflow:hidden;
+      word-wrap: break-word;
+      margin-bottom:5px;
+      cursor: pointer;
+   }
    & .career{
       background-color:${({ theme }) => theme.red};
+      height:100%;
    }
    & .conference{
       background-color:${({ theme }) => theme.blue};
@@ -63,35 +65,35 @@ const EventTag = ({ openViewEventModal, eventInfo }) => {
    if(eventInfo.type === 'Careers event'){
       return(
          <Wrapper onClick={handleClick}>
-            <p className="career">{eventInfo.title}</p>
+            <p className="tag career">{eventInfo.title}</p>
          </Wrapper>
       )
    }
    else if(eventInfo.type === 'Conference'){
       return(
          <Wrapper onClick={handleClick}>
-            <p className="conference">{eventInfo.title}</p>
+            <p className="tag conference">{eventInfo.title}</p>
          </Wrapper>
       )
    }
    else if(eventInfo.type === 'Special interest talk'){
       return(
          <Wrapper onClick={handleClick}>
-            <p className="special">{eventInfo.title}</p>
+            <p className="tag special">{eventInfo.title}</p>
          </Wrapper>
       )
    }
    else if(eventInfo.type === 'Revision'){
       return(
          <Wrapper onClick={handleClick}>
-            <p className="revision">{eventInfo.title}</p>
+            <p className="tag revision">{eventInfo.title}</p>
          </Wrapper>
       )
    }
    else if(eventInfo.type === 'Other'){
       return(
          <Wrapper onClick={handleClick}>
-            <p className="other">{eventInfo.title}</p>
+            <p className="tag other">{eventInfo.title}</p>
          </Wrapper>
       )
    }
