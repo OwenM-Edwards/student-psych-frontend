0defe188e3b04508fbdb01c4a2a066caee939749
Zibbly <owenhedwards@gmail.com>
2021-02-26 17:35:50 +0000

    Create PinnedEvents.js

---
+++ src/components/PinnedEvents.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import styled from "styled-components";
+
+
+
+const Wrapper = styled.div`
+   width:100%;
+   height:100%;
+   display:flex;
+   justify-content:center;
+   flex-direction:row;
+`
+
+const PinnedEvents = () => {
+
+
+
+   return(
+      <Wrapper>
+         Pinned Events.
+      </Wrapper>
+   )  
+  
+}
+
+
+export default PinnedEvents;
