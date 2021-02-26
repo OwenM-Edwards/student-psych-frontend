12875a0ce10b88c91e417eaa7be32be9489fe910
Zibbly <owenhedwards@gmail.com>
2021-02-26 17:35:46 +0000

    Create AccountSettings.js

---
+++ src/components/AccountSettings.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import styled from "styled-components";
+
+
+
+const Wrapper = styled.div`
+   width:100%;
+   height:100%;
+   display:flex;
+   justify-content:center;
+   flex-direction:row;
+`
+
+const AccountSettings = () => {
+
+
+
+   return(
+      <Wrapper>
+         Account Settings.
+      </Wrapper>
+   )  
+  
+}
+
+
+export default AccountSettings;
