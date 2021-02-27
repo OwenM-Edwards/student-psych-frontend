39ee3befb04a0d300263a9739f99726fb46b8d4a
Zibbly <sirflapdragon@gmail.com>
2021-02-27 13:29:45 +0000

    Create ChangePassword.js

---
+++ src/components/ChangePassword.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+   width:100%;
+   height:100%;
+   display:flex;
+   justify-content:center;
+   flex-direction:row;
+`
+
+
+const ChangePassword = () => {
+   return(
+        <Wrapper>
+            Password Change
+        </Wrapper>
+   )  
+}
+
+
+export default ChangePassword;
