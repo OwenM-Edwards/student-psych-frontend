99929a74c40b86e25ca18f0012c540afdebd3f3d
Zibbly <owenhedwards@gmail.com>
2021-03-04 22:31:47 +0000

    Update Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,17 +2,20 @@ import React, { useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { connect } from "react-redux";
-import { getSecureEventInfo,deleteEntry, editEntry,searchEntries,modalHandler } from "../redux/actions/index";
+import { getSecureEventInfo,deleteEntry, editEntry,searchEntries,modalHandler,selectDate } from "../redux/actions/index";
 import { LoadingIcon, EditEventModal, EventModal, AddEventModal } from '../components/index';
 import { toast } from "react-toastify";
 import { useHistory } from 'react-router-dom';
+import { rightIconDarkMode } from "../assets/index.js";
+
+
 const Wrapper = styled.div`
    width:100%;
    height:100%;
    display:flex;
    padding:50px 50px 20px 50px;
    justify-content:center;
-   box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+
    & .frosted{
       box-shadow: inset 0 0 500px rgba(255, 255, 255, .1);
       filter: blur(2px);
@@ -21,7 +24,7 @@ const Wrapper = styled.div`
 
 const StyledEntriesContainer = styled.div `
    width:100%;
-   max-width: 1400px;
+   max-width: 900px;
    height:100%;
    display:flex;
    flex-direction:column;
@@ -29,22 +32,35 @@ const StyledEntriesContainer = styled.div `
    border-radius:3px;
    padding:20px;
    overflow-y:scroll;
-   
-
+   background: ${({ theme }) => theme.primary.main};
 `
-const StyledEntry = styled.p`
-   min-width:100%;
-   max-width: 70%;
-   background: ${({ theme }) => theme.backgroundLight};
-   color: ${({ theme }) => theme.contrastText};
+const StyledEntry = styled.div`
+   width: 100%;
    height:auto;
    margin-bottom:10px;
-   padding:10px 20px 10px 20px;
-   cursor:pointer;
-   border-radius:5px;
    transition: all 0.1s ease-in-out;
-   &:hover {
-      scale:0.99;
+   display:flex;
+   flex-direction:row;
+   & div {
+      background: ${({ theme }) => theme.backgroundLight};
+      color: ${({ theme }) => theme.contrastText};
+      width:100%;
+      cursor:pointer;
+      padding:10px 20px 10px 20px;
+      border-radius:5px;
+      display:flex;
+      flex-direction:row;
+      flex-wrap:wrap;
+      grid-gap:10px;
+   }
+   & img {
+      align-self:center;
+      height:50px;
+      cursor:pointer;
+      transition: all 0.1s ease-in-out;
+      &:hover {
+         scale:0.90;
+      }
    }
 `
 
@@ -56,7 +72,9 @@ const Search = ({
       entriesState, 
       editEntryState, 
       deleteEntryState,
-      modalHandler
+      modalHandler,
+      selectDate,
+      selectedDate, 
    }) => {
 
    const {searchterm} = useParams();
@@ -86,9 +104,23 @@ const Search = ({
       let count = 1;
       if(entriesState.entries){
          entriesState.entries.forEach(entry => {
+            let linkDate = new Date(entry.year, entry.month -1);
             sortedEntries.push(
                <StyledEntry onClick={()=>openViewEventModal(entry)} key={count}> 
-                  Title:{entry.title} Organisation:{entry.organisation}
+                  <div>
+                     <p>Title: {entry.title} </p>
+                     <p>Organisation: {entry.organisation} </p>
+                     <p>Start Time: {entry.starttime}</p>
+                     <p>End Time: {entry.endtime} </p>
+                     <p>Day: {entry.day}</p>
+                     <p>Month: {entry.month}</p>
+                     <p>Year: {entry.year}</p>
+                  </div>
+                  <img onClick={()=>{
+                     selectDate(false)
+                     history.push(`/calendar/${linkDate.getMonth() +1}/${linkDate.getFullYear()}`)
+                     }} src={rightIconDarkMode}
+                  />
                </StyledEntry>
             )
             count++;
@@ -143,5 +175,5 @@ const Search = ({
 }
 
 
-const mapStateToProps = (state) => ({ modalState:state.modal, editEntryState:state.editEntry, deleteEntryState:state.deleteEntry, auth:state.authenticate, entriesState:state.searchEntries });
-export default connect(mapStateToProps, {modalHandler, getSecureEventInfo,deleteEntry, editEntry,searchEntries})(Search);
+const mapStateToProps = (state) => ({ selectedDate:state.selectedDate.selectedDate, modalState:state.modal, editEntryState:state.editEntry, deleteEntryState:state.deleteEntry, auth:state.authenticate, entriesState:state.searchEntries });
+export default connect(mapStateToProps, {selectDate, modalHandler, getSecureEventInfo,deleteEntry, editEntry,searchEntries})(Search);
