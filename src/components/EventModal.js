d2bb6e450825d8f93bf04f07300669082005693e
Zibbly <owenhedwards@gmail.com>
2021-03-10 16:04:52 +0000

    Update EventModal.js

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -63,15 +63,15 @@ const Wrapper = styled.div`
       }
    }
    & .eventDescription {
-      width:80%;
+      width:88%;
       margin-top:5px;
       height:auto;
       box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
       transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-      padding:20px;
+      padding:10px;
       border-radius:10px;
       position: relative;
-      left:30px;
+      left:25px;
       margin-bottom:20px;
    }
 `
