fdc7b974f458f31df5c3fb5208c91e6080e57604
Zibbly <owenhedwards@gmail.com>
2021-04-08 02:10:05 +0100

    Styling for change password form, add info text about adding events.

--- src/components/AccountSettings.js
+++ src/components/AccountSettings.js
@@ -8,24 +8,58 @@ import { LoadingIcon } from './index';
 
 const Wrapper = styled.div`
    width:100%;
-   height:100%;
+   height:auto;
+   min-height:100%;
    display:flex;
    flex-direction:column;
    padding:30px;
    border-radius:0 0 5px 5px;
    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23); 
    background: ${({ theme }) => theme.primary.main};
+   align-items:center;
    & h2 {
       margin-bottom:30px;
    }
    & form {
       width:100%;
       max-width: 600px;
+   }
+   & fieldset {
       display:flex;
-      flex-direction:columm;
+      justify-content:center;
+      flex-direction:column;
+      padding:10px;
+      padding-top:20px;
+      border-radius:10px;
+      border:0px;
+      text-align:center;
+      font-size:1.2rem;
    }
    & input {
+      padding:10px;
       width:100%;
+      margin: 0 auto;
+      background-color:${({ theme }) => theme.primary.offWhite};
+      border-radius:5px;
+      margin-bottom:10px;
+      outline: none;
+      border:0px;
+   }
+   & .submitButton {
+      padding:10px;
+      width:100%;
+      background-color:#2f3e4d;
+      border-radius:5px;
+      color:${({ theme }) => theme.primary.light};
+      border:0px;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+      font-size:1.1rem;
+      cursor: pointer;
+      transition: all 0.2s ease 0s;
+      &:hover {
+         background-color:${({ theme }) => theme.primary.light};
+         color:${({ theme }) => theme.primary.offBlack};
+      }
    }
 `
 
@@ -34,6 +68,11 @@ const DeleteModal = styled.div`
    height:300px;
    background-color:red;
    position: absolute;
+`
+
+const InfoWrapper = styled.div`
+   margin-top:50px;
+
 `
 
 const AccountSettings = ({changePassword, changePasswordState, deleteOwnAccount}) => {
@@ -80,6 +119,7 @@ const AccountSettings = ({changePassword, changePasswordState, deleteOwnAccount}
          
          <h2>Account Settings</h2>
          <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset>
                <input
                type="text"
                placeholder="New password."
@@ -97,10 +137,18 @@ const AccountSettings = ({changePassword, changePasswordState, deleteOwnAccount}
                   maxLength="50"
                   ref={register({ required:true })}
                />
-               <input value="Change password" type="submit" />
+               <input className="submitButton" value="Change password" type="submit" />
+            </fieldset>
+            
          </form>
 
          {/* <button onClick={()=>setDeleteModalToggle(true)}>Delete Account</button> */}
+
+
+         <InfoWrapper>
+            <p>Event organiser? You’ll need to be approved by our admins before you can add your own events. </p>
+            <p> Get in touch with us via: <a href="mailto:admin@studentpsychiatry.co.uk">admin@studentpsychiatry.co.uk</a></p>
+         </InfoWrapper>
       </Wrapper>
    )  
   
