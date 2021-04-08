72a2b41920aa2be80c0c269ee6a66648c29381c2
Zibbly <owenhedwards@gmail.com>
2021-04-08 02:19:07 +0100

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 
 const api = axios.create({
    // baseURL: 'http://localhost:3000/',
-   baseURL: 'http://api.studentpsychiatry.co.uk/', 
+   baseURL: 'https://student-psych-api.herokuapp.com/', 
    withCredentials: true,
 }); 
 
